289ddcc809f61a7a61a173152da878319990d768
Oscar Lindqvist <olindqv@gmail.com>
2021-04-13 12:17:27 +0200

    setup typescript config file, source and build dir

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello world');
+import express from 'express';
+import path from 'path';
+import helmet from 'helmet';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const port: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+
+app.use(helmet());
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Testing the API - compiled from typescript' });
+});
+
+app.listen(port, () => console.log(`App is listening on port ${port}`));
