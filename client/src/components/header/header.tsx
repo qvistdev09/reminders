6e9cfd99cfc6e4ad7c014be9bcec325a058f075f
Oscar Lindqvist <olindqv@gmail.com>
2021-04-15 10:21:22 +0200

    style user icon

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -2,19 +2,35 @@ import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useOktaAuth } from '@okta/okta-react';
 import { Link } from 'react-router-dom';
+
+// components
 import Icon from '../icon/icon';
+import UserIcon from '../user-icon/user-icon';
+
+// redux
+import { useAppDispatch } from '../../hooks/redux-hooks';
+import { useAppSelector } from '../../hooks/redux-hooks';
+import { setName } from '../../reducers/slices/user-details';
+import { getUserDetails } from '../../reducers/slices/user-details';
 
 const Header = () => {
+  const dispatch = useAppDispatch();
+  const userDetails = useAppSelector(getUserDetails);
   const { authState, oktaAuth } = useOktaAuth();
   const history = useHistory();
 
   const authed = authState.isAuthenticated;
 
   useEffect(() => {
-    if (authState.isAuthenticated) {
-      oktaAuth.token.getUserInfo().then(info => console.log(info, 'hey'));
+    if (authState.isAuthenticated && !userDetails.retrieved) {
+      oktaAuth.token.getUserInfo().then(info => {
+        const { given_name: firstName, family_name: lastName } = info;
+        if (firstName && lastName) {
+          dispatch(setName({ firstName, lastName }));
+        }
+      });
     }
-  }, [authState.isAuthenticated, oktaAuth]);
+  }, [authState.isAuthenticated, oktaAuth, dispatch, userDetails.retrieved]);
 
   const loginOrLogout = authed ? (
     <button className='header__btn' onClick={() => oktaAuth.signOut()}>
@@ -44,6 +60,9 @@ const Header = () => {
             </button>
           )}
           {loginOrLogout}
+          {userDetails.retrieved && (
+            <UserIcon firstName={userDetails.firstName} lastName={userDetails.lastName} />
+          )}
         </div>
       </nav>
     </header>
--- client/src/components/user-icon/user-icon.tsx
+++ client/src/components/user-icon/user-icon.tsx
@@ -5,7 +5,11 @@ interface Props {
 
 const UserIcon = ({ firstName = '', lastName = '' }: Props) => {
   const initials = firstName === '' || lastName === '' ? '?' : firstName[0] + lastName[0];
-  return <div className='user-icon'>{initials}</div>;
+  return (
+    <div className='user-icon'>
+      <p className='user-icon__initials'>{initials}</p>
+    </div>
+  );
 };
 
 export default UserIcon;
