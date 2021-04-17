7d44a27d401c0cc80cc48bef1520a3ed88deb1d5
Oscar Lindqvist <olindqv@gmail.com>
2021-04-17 13:36:04 +0200

    close search suggestions when name is clicked

--- client/src/components/form/elements/form-search-wrapper.tsx
+++ client/src/components/form/elements/form-search-wrapper.tsx
@@ -6,18 +6,38 @@ interface Props {
   children: React.ReactNode;
 }
 
-const FormSearchWrapper = ({ suggestions, render, children }: Props) => (
-  <div
-    className={
-      suggestions.length > 0
-        ? 'form__search-wrapper form__search-wrapper--expanded'
-        : 'form__search-wrapper'
-    }
-  >
-    {children}
-    {suggestions.length > 0 && (
-      <div className='form__search-wrapper-suggestions'>{render(suggestions)}</div>
-    )}
-  </div>
-);
+const FormSearchWrapper = ({ suggestions, render, children }: Props) => {
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  useEffect(() => {
+    const show = (e: Event) => {
+      const clicked = e.target;
+      if (clicked instanceof HTMLElement) {
+        if (clicked.classList.contains('form__input')) {
+          return setShowSuggestions(true);
+        }
+        setShowSuggestions(false);
+      }
+    };
+    document.body.addEventListener('click', show);
+    return () => {
+      document.body.removeEventListener('click', show);
+    };
+  }, []);
+
+  return (
+    <div
+      className={
+        suggestions.length > 0 && showSuggestions
+          ? 'form__search-wrapper form__search-wrapper--expanded'
+          : 'form__search-wrapper'
+      }
+    >
+      {children}
+      {suggestions.length > 0 && showSuggestions && (
+        <div className='form__search-wrapper-suggestions'>{render(suggestions)}</div>
+      )}
+    </div>
+  );
+};
 export default FormSearchWrapper;
