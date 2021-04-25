71c40e9551db7b3fd5ee3ffd3049843f3098d876
Oscar Lindqvist <olindqv@gmail.com>
2021-04-25 22:54:52 +0200

    add spinner to shared projects

--- client/src/components/pages/shared-with-you.tsx
+++ client/src/components/pages/shared-with-you.tsx
@@ -1,15 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ProjectObject } from 'reminders-shared/sharedTypes';
 import { projectsApi } from '../../api-service/projects';
 import { useAccessToken } from '../../hooks/use-access-token';
 import { Card } from '../presentational/containers/card';
 import { Flex } from '../presentational/containers/flex';
+import { Spinner } from '../presentational/spinner/spinner';
 import { Text } from '../presentational/texts/text';
 import { ProjectCard } from '../project-card';
 
 let mounted = true;
 const SharedWithYou = () => {
   const accessToken = useAccessToken();
+  const [fetchDone, setFetchDone] = useState(false);
   const [projects, setProjects] = useState<ProjectObject[]>([]);
 
   useEffect(() => {
@@ -18,6 +20,7 @@ const SharedWithYou = () => {
       projectsApi.getAll(accessToken, 'others').then(res => {
         if (mounted) {
           setProjects(res.data.projects);
+          setFetchDone(true);
         }
       });
     }
@@ -30,17 +33,19 @@ const SharedWithYou = () => {
     <Flex justify='center' flex={1} align='start' bg='darkenedBg'>
       <Flex maxWidth='appMaxWidth' align='start' yPadding={0} childrenGap='big' xPadding={1}>
         <Card header='Shared with you' flex={0.3}>
-          <Text>You have been invited to:</Text>
-          <Text weight='strong'>{projects.length.toString()} projects</Text>
+          {fetchDone && <Text>You have been invited to:</Text>}
+          {fetchDone && <Text weight='strong'>{projects.length.toString()} projects</Text>}
         </Card>
         <Flex direction='column' align='stretch' childrenGap='big' flex={0.7}>
-          {projects.length > 0 &&
+          {fetchDone &&
+            projects.length > 0 &&
             projects.map(project => <ProjectCard key={project.projectId} project={project} owned={false} />)}
-          {projects.length === 0 && (
+          {fetchDone && projects.length === 0 && (
             <Card>
               <Text>You have not been invited to any projects yet.</Text>
             </Card>
           )}
+          {!fetchDone && <Spinner justify='center'>Loading shared projects...</Spinner>}
         </Flex>
       </Flex>
     </Flex>
